319f5e40643befccda7e4d24e6c9b9c05086aa91
Jecintawanjiru <jecintawanjiru0@gmail.com>
2014-03-10 19:19:17 +0300

    Update test.php

--- test.php
+++ test.php
@@ -1,1 +1,15 @@
- <?php phpinfo(); ?>
+<?php
+
+<?php 
+ $friend[0] = "Justin"; 
+ $friend[1] = "Lloyd"; 
+ $friend[2] = "Alexa"; 
+ $friend[3] = "Devron"; 
+
+$age["Justin"] = 45; $age["Lloyd"] = 32; $age["Alexa"] = 26; $age["Devron"] = 15;
+
+print "My friends names are " . $friend[0] . ", " . $friend[1] . ", " . $friend[2] . ", and " . $friend[3];
+
+print "<p>"; 
+
+?>
